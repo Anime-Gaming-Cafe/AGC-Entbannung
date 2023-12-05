e37871e50b4dfc83c2976bdd68c1df3dd65ba9d1
Fabi-Chan <fabi@fabi-chan.me>
2023-12-05 02:01:08 +0100

    Refactor

--- src/Eventhandler/UnbanGuild/onComponentInteraction.cs
+++ src/Eventhandler/UnbanGuild/onComponentInteraction.cs
@@ -3,6 +3,7 @@ using DisCatSharp;
 using DisCatSharp.ApplicationCommands;
 using DisCatSharp.Entities;
 using DisCatSharp.Enums;
+
 using DisCatSharp.EventArgs;
 using DisCatSharp.Exceptions;
 
@@ -19,41 +20,10 @@ public class onComponentInteraction : ApplicationCommandsModule
             string cid = e.Interaction.Data.CustomId;
             if (cid == "open_appealticketinfo")
             {
-                // check if user isbanned
-                DiscordGuild mainGuild = await client.GetGuildAsync(GlobalProperties.MainGuildId);
-                bool isBanned = false;
-                try
-                {
-                    await mainGuild.GetBanAsync(e.User.Id);
-                    isBanned = true;
-                }
-                catch (NotFoundException)
-                {
-                    // ignored
-                    isBanned = false;
-                }
-                catch (Exception exception)
-                {
-                    var embed = new DiscordEmbedBuilder();
-                    embed.WithTitle("Fehler!");
-                    embed.WithDescription("Es ist ein Fehler aufgetreten. Bitte versuche es später erneut. Der Fehler wurde automatisch an den Entwickler weitergeleitet.");
-                    embed.WithColor(DiscordColor.Red);
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral());
-                    await ErrorReporting.SendErrorToDev(client, e.User, exception);
-                }
-                
-                if (!isBanned)
-                {
-                    var embed = new DiscordEmbedBuilder();
-                    embed.WithTitle("Nicht gebannt!");
-                    embed.WithDescription("Wie es scheint, bist du nicht auf AGC gebannt. Diese Überprüfung ist automatisiert. Du kannst also keinen Entbannungsantrag stellen.");
-                    embed.WithColor(DiscordColor.Red);
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral());
-                }
+                await Methodes.PanelCheckBan(client, e);
             }
             return Task.CompletedTask;
         });
     }
-
-
+    
 }
---
+++ src/Helpers/InteractionMethodes/Methodes.cs
@@ -0,0 +1,49 @@
+﻿using DisCatSharp;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using DisCatSharp.EventArgs;
+using DisCatSharp.Exceptions;
+
+namespace AGC_Entbannungssystem.Helpers;
+
+public static class Methodes
+{
+    public static async Task PanelCheckBan(DiscordClient client, ComponentInteractionCreateEventArgs e)
+    {
+        // check if user isbanned
+        DiscordGuild mainGuild = await client.GetGuildAsync(GlobalProperties.MainGuildId);
+        bool isBanned = false;
+        try
+        {
+            await mainGuild.GetBanAsync(e.User.Id);
+            isBanned = true;
+        }
+        catch (NotFoundException)
+        {
+            // ignored
+            isBanned = false;
+        }
+        catch (Exception exception)
+        {
+            var embed = new DiscordEmbedBuilder();
+            embed.WithTitle("Fehler!");
+            embed.WithDescription(
+                "Es ist ein Fehler aufgetreten. Bitte versuche es später erneut. Der Fehler wurde automatisch an den Entwickler weitergeleitet.");
+            embed.WithColor(DiscordColor.Red);
+            await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral());
+            await ErrorReporting.SendErrorToDev(client, e.User, exception);
+        }
+
+        if (!isBanned)
+        {
+            var embed = new DiscordEmbedBuilder();
+            embed.WithTitle("Nicht gebannt!");
+            embed.WithDescription(
+                "Wie es scheint, bist du nicht auf AGC gebannt. Diese Überprüfung ist automatisiert. Du kannst also keinen Entbannungsantrag stellen.");
+            embed.WithColor(DiscordColor.Red);
+            await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral());
+        }
+    }
+}
