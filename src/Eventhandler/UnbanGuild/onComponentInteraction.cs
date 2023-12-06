415416ae9b4a5c23dcadac8e0214ba834ff65f63
Fabi-Chan <fabi@fabi-chan.me>
2023-12-06 01:07:10 +0100

    Add response

--- src/Eventhandler/UnbanGuild/onComponentInteraction.cs
+++ src/Eventhandler/UnbanGuild/onComponentInteraction.cs
@@ -1,10 +1,14 @@
 ﻿#region
 
 using AGC_Entbannungssystem.Helpers;
+using AGC_Entbannungssystem.Services;
 using DisCatSharp;
 using DisCatSharp.ApplicationCommands;
+using DisCatSharp.Entities;
 using DisCatSharp.Enums;
 using DisCatSharp.EventArgs;
+using DisCatSharp.Exceptions;
+using Sentry;
 
 #endregion
 
@@ -21,10 +25,102 @@ public class onComponentInteraction : ApplicationCommandsModule
             string cid = e.Interaction.Data.CustomId;
             if (cid == "open_appealticketinfo")
             {
-                await Methodes.PanelCheckBan(client, e);
+                DiscordGuild mainGuild = await client.GetGuildAsync(GlobalProperties.MainGuildId);
+                bool isBanned = false;
+                try
+                {
+                    await mainGuild.GetBanAsync(e.User.Id);
+                    isBanned = true;
+                }
+                catch (NotFoundException)
+                {
+                    // ignored
+                    isBanned = false;
+                }
+                catch (Exception exception)
+                {
+                    var embed = new DiscordEmbedBuilder();
+                    embed.WithTitle("Fehler!");
+                    embed.WithDescription(
+                        "Es ist ein Fehler aufgetreten. Bitte versuche es später erneut. Der Fehler wurde automatisch an den Entwickler weitergeleitet.");
+                    embed.WithColor(DiscordColor.Red);
+                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                        new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral());
+                    await ErrorReporting.SendErrorToDev(client, e.User, exception);
+                }
+                if (e.User.Id == GlobalProperties.BotOwnerId)
+                {
+                    // application test
+                    isBanned = true;
+                }
+                if (!isBanned)
+                {
+                    var embed = new DiscordEmbedBuilder();
+                    embed.WithTitle("Nicht gebannt!");
+                    embed.WithDescription(
+                        "Wie es scheint, bist du nicht auf AGC gebannt. Diese Überprüfung ist automatisiert. Du kannst also keinen Entbannungsantrag stellen.");
+                    embed.WithColor(DiscordColor.Red);
+                    await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                        new DiscordInteractionResponseBuilder().AddEmbed(embed).AsEphemeral());
+                    return;
+                }
+
+                var rb = new DiscordInteractionResponseBuilder();
+                var button = new DiscordButtonComponent(ButtonStyle.Success, "open_appealticket_confirm",
+                    "Ich habe alles gelesen und verstanden!",
+                    emoji: new DiscordComponentEmoji("✅"));
+                rb.AddComponents(button);
+                rb.AddEmbeds(MessageGenerator.UnbanNoteGenerate());
+                rb.AsEphemeral();
+                await e.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, rb);
+                try
+                {
+                    ulong logChannelId = ulong.Parse(BotConfigurator.GetConfig("MainConfig", "LogChannelId"));
+                    var logChannel = await client.GetChannelAsync(logChannelId);
+                    await logChannel.SendMessageAsync(
+                        $"{e.User.Mention} ({e.User.Id}) hat die Antragshinweise **geöffnet** - {DateTime.Now.Timestamp(TimestampFormat.ShortDateTime)}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+            else if (cid == "open_appealticket_confirm")
+            {
+                // TODO: check if user has already an open ticket
+                // TODO: check if user is "still" banned
+                ulong logChannelId = ulong.Parse(BotConfigurator.GetConfig("MainConfig", "LogChannelId"));
+                var logChannel = await client.GetChannelAsync(logChannelId);
+                await logChannel.SendMessageAsync(
+                    $"{e.User.Mention} ({e.User.Id}) hat die Antragshinweise **akzeptiert** - {DateTime.Now.Timestamp(TimestampFormat.ShortDateTime)}");
+                await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral());
+                await e.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent("Ticket wird erstellt..."));
+                var appealrole = e.Guild.GetRole(ulong.Parse(BotConfigurator.GetConfig("MainConfig", "AppealRoleId")));
+                await Task.Delay(1000);
+                await e.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent("Prüfe auf offenes Ticket..."));
+                DiscordMember member = await e.Guild.GetMemberAsync(e.User.Id);
+                if (member.Roles.Contains(appealrole))
+                {
+                    var embed = new DiscordEmbedBuilder();
+                    embed.WithTitle("Fehler!");
+                    embed.WithDescription(
+                        "Du hast bereits ein offenes Ticket. Bitte nutze dieses, um einen Entbannungsantrag zu stellen.");
+                    embed.WithColor(DiscordColor.Red);
+                    await e.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
+                    return;
+                }
+                await Task.Delay(1000);
+                await e.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent("Erstelle Ticket..."));
+                await logChannel.SendMessageAsync(
+                    $"$new {e.User.Id}");
+                await Task.Delay(500);
+                await e.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent("Ticket erstellt!"));
+                
             }
 
-            return Task.CompletedTask;
+
+
+            await Task.CompletedTask;
         });
     }
 }
---
+++ src/Helpers/MessageGenerator.cs
@@ -0,0 +1,42 @@
+﻿using DisCatSharp.Entities;
+
+namespace AGC_Entbannungssystem.Helpers;
+
+public static class MessageGenerator
+{
+    public static List<DiscordEmbed> UnbanNoteGenerate()
+    {
+        var color = new DiscordColor("2f3136");
+        var embeds = new List<DiscordEmbed>();
+        var embed1 = new DiscordEmbedBuilder();
+        embed1.WithTitle("Warum wurde ich gebannt?");
+        embed1.WithDescription("Du wurdest gebannt, weil du auf AGC gegen die Regeln verstoßen hast. \n" +
+                               "Sofern du nicht weißt, wieso du gebannt wurdest, erfrage bitte dies gleich zum Anfang deines Entbannungsantrags. Wir geben dir dann Auskunft darüber, wieso du gebannt wurdest. \n\n" +
+                               "- Die Erfragung des Grundes fließt nicht in die Bewertung deines Entbannungsantrags ein. \n\n" +
+                               "> ⚠️ Wenn du denkst, dass du zu unrecht gebannt wurdest, teile uns dies bitte __zu Beginn__ deines Antrags mit!");
+        embed1.WithColor(color);
+        embeds.Add(embed1.Build());
+        var embed2 = new DiscordEmbedBuilder();
+        embed2.WithTitle("Antragsstellung");
+        embed2.WithDescription(
+            "Es wird von dir erwartet, dass du dich umfassend zu deinem Bann äußerst. Außerdem erwarten wir, dass du folgende Fragen ausführlich beantwortest: \n" +
+            "- Wieso wurdest du gebannt bzw hast gegen unsere Serverregeln verstoßen? \n" +
+            "- Was hast du daraus gelernt? \n" +
+            "- Was wirst du anders machen, um so eine Situation in Zukunft zu vermeiden? \n" +
+            "- Wie können wir sicher sein, dass du dich in Zukunft an unsere Regeln hältst? \n");
+        embed2.WithColor(color);
+        embeds.Add(embed2.Build());
+        var embed3 = new DiscordEmbedBuilder();
+        embed3.WithTitle("Wichtig!");
+        embed3.WithDescription(
+            "Unser Team achtet auf eine angemessene Ausdrucksweise. Achte auch so gut wie möglich auf Rechtschreibung und Grammatik. \n" +
+            "Verzichte bitte auf mögliche Lückenfüller, indem du auf unnötige Rechtfertigungen verzichtest, wie z.B. warum du entbannt werden möchtest. \n\n" +
+            "> Solltest du Schwierigkeiten mit der Rechtschreibung haben, durch z.b eine Lese- und Rechtschreibschwäche, so kannst du dies gerne in deinem Antrag erwähnen. \n\n" +
+            "Nimm dir außerdem Zeit. Wir möchten sehen, dass dir etwas an der Entbannung liegt. Schau also dass du alle Kriterien erfüllst. \n\n" +
+            "⚠️ Wenn der Entbannungsantrag nicht ordentlich bearbeitet wird, wird er abgelehnt! \n\n" +
+            "🛑 Sollte dein Antrag abgelehnt werden, wirst du in der Regel für 3 Monate gesperrt. ");
+        embed3.WithColor(color);
+        embeds.Add(embed3.Build());
+        return embeds;
+    }
+}
