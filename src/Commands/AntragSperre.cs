b22b2a3e6ddb2971b7529a09822b5bcee337c37a
Fabi-Chan <fabi@fabi-chan.me>
2023-12-07 12:58:07 +0100

    Add sperre command TODO: event

---
+++ src/Commands/AntragSperre.cs
@@ -0,0 +1,97 @@
+﻿using AGC_Entbannungssystem.Enums;
+using AGC_Entbannungssystem.Helpers;
+using AGC_Entbannungssystem.Services;
+using DisCatSharp;
+using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace AGC_Entbannungssystem.Commands;
+
+public class AntragSperre : ApplicationCommandsModule
+{
+    [ApplicationRequireStaffRole]
+    [SlashCommand("sperre", "Sperrt einen User von der Antragstellung für eine bestimmte Zeit.")]
+    public static async Task SperreCommand(InteractionContext ctx,
+        [Option("user", "Der User, der gesperrt werden soll.")] DiscordUser user, [Option("antragsnummer", "Die Antragsnummer.")] int antragsnummer,
+        [Option("grund", "Der Grund für die Sperre.")] string reason,
+        [Option("dauer", "Die Zeit, für die der User gesperrt werden soll.")] SperrDauer zeit = SperrDauer.DreiMonate)
+    {
+        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().AsEphemeral());
+        var constring = Helperfunctions.DbString();
+        ulong roleid = ulong.Parse(BotConfigurator.GetConfig("MainConfig", "SperreRoleId"));
+        DiscordRole role = ctx.Guild.GetRole(roleid);
+        await ctx.EditResponseAsync(
+            new DiscordWebhookBuilder().WithContent("Prüfe, ob der User bereits gesperrt ist..."));
+        await using var con = new NpgsqlConnection(constring);
+        await con.OpenAsync();
+        await using var cmd = new NpgsqlCommand("SELECT * FROM antragssperre WHERE user_id = @userid", con);
+        cmd.Parameters.AddWithValue("userid", (long)user.Id);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        if (reader.HasRows)
+        {
+            await ctx.EditResponseAsync(
+                new DiscordWebhookBuilder().WithContent("⚠️ Der User ist bereits gesperrt!"));
+            return;
+        }
+        
+        await con.CloseAsync();
+        
+        await ctx.EditResponseAsync(
+            new DiscordWebhookBuilder().WithContent("Der User ist nicht gesperrt. Sperre den User für die angegebene Zeit..."));
+        try
+        {
+            DiscordMember member = await ctx.Guild.GetMemberAsync(user.Id);
+            await member.GrantRoleAsync(role, "Antragssperre");
+            await ctx.EditResponseAsync(
+                new DiscordWebhookBuilder().WithContent($"✅ Der User wurde gesperrt!"));
+        }
+        catch (Exception e)
+        {
+            ctx.Client.Logger.LogError(e, "Error while granting role to user. User is not in guild.");
+        }
+        var timestamp = GetSperrDauer(zeit);
+        await using var con2 = new NpgsqlConnection(constring);
+        await con2.OpenAsync();
+        await using var cmd2 = new NpgsqlCommand("INSERT INTO antragssperre (user_id, reason, expires_at) VALUES (@userid, @reason, @timestamp)", con2);
+        cmd2.Parameters.AddWithValue("userid", (long)user.Id);
+        cmd2.Parameters.AddWithValue("reason", reason);
+        cmd2.Parameters.AddWithValue("timestamp", timestamp);
+        await cmd2.ExecuteNonQueryAsync();
+        await con2.CloseAsync();
+        await ctx.EditResponseAsync(
+            new DiscordWebhookBuilder().WithContent($"✅ Die Sperre wurde eingetragen!"));
+        DiscordEmbedBuilder embed = new DiscordEmbedBuilder();
+        embed.WithDescription($"{DateTime.UtcNow.Timestamp(TimestampFormat.LongDateTime)} - {user.Mention} ({user.Id}) - Antrag {antragsnummer} - ``{reason}`` -> Gesperrt bis: <t:{timestamp}:f> ( <t:{timestamp}:R> )");
+        embed.WithFooter($"Gesperrt durch {ctx.User.UsernameWithDiscriminator} ({ctx.User.Id})", ctx.User.AvatarUrl);
+        ulong infochannelid = ulong.Parse(BotConfigurator.GetConfig("MainConfig", "SperreInfoChannelId"));
+        DiscordChannel ichan = ctx.Guild.GetChannel(infochannelid);
+        await ichan.SendMessageAsync(embed);
+    }
+    
+    private static long GetSperrDauer(SperrDauer dauer)
+    {
+        long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        switch (dauer)
+        {
+            case SperrDauer.DreiMonate:
+                timestamp += 7776000;
+                break;
+            case SperrDauer.SechsMonate:
+                timestamp += 15552000;
+                break;
+            case SperrDauer.EinJahr:
+                timestamp += 31536000;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(dauer), dauer, null);
+        }
+        return timestamp;
+    }
+
+}
---
+++ src/Enums/SperrDauer.cs
@@ -0,0 +1,15 @@
+﻿using DisCatSharp.ApplicationCommands.Attributes;
+
+namespace AGC_Entbannungssystem.Enums;
+
+public enum SperrDauer
+{
+    [ChoiceName("3 Monate")]
+    DreiMonate,
+    [ChoiceName("6 Monate (Nur Absprache mit dem Team)")]
+    SechsMonate,
+    [ChoiceName("1 Jahr (Nur Absprache mit dem Team)")]
+    EinJahr,
+    [ChoiceName("Permanent (Nur Absprache mit dem Team)")]
+    Permanent,
+}
