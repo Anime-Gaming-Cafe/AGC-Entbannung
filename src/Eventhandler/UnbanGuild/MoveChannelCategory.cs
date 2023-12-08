70d25e7498243d1629e4a5540bc8af6cf8fedfbb
Fabi-Chan <fabi@fabi-chan.me>
2023-12-08 01:32:55 +0100

    Add move antrag

---
+++ src/Eventhandler/UnbanGuild/MoveChannelCategory.cs
@@ -0,0 +1,58 @@
+﻿using AGC_Entbannungssystem.Helpers;
+using AGC_Entbannungssystem.Services;
+using DisCatSharp;
+using DisCatSharp.ApplicationCommands;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using DisCatSharp.EventArgs;
+using Npgsql;
+
+namespace AGC_Entbannungssystem.Eventhandler.UnbanGuild;
+
+
+[EventHandler]
+public class MoveChannelCategory : ApplicationCommandsModule
+{
+    [Event]
+    public async Task MessageCreated(DiscordClient client, MessageCreateEventArgs e)
+    {
+        _ = Task.Run(async () =>
+        {
+            List<string> keywords = new()
+            {
+                "accept",
+                "deny",
+                "deny2",
+                "deny24",
+            };
+            // check if guild is unban guild
+            if (e.Guild.Id != GlobalProperties.UnbanServerId) return;
+            if (e.Channel.Name.StartsWith("antrag-") && !e.Channel.Name.EndsWith("-geschlossen"))
+            {
+                if (e.Message.Author.IsBot) return;
+                ulong moderatorRoleId = ulong.Parse(BotConfigurator.GetConfig("MainConfig", "UnbanGuildTeamRoleId"));
+                DiscordRole moderatorRole = e.Guild.GetRole(moderatorRoleId);
+                var member = await e.Guild.GetMemberAsync(e.Message.Author.Id);
+                if (member.Roles.Contains(moderatorRole)) return;
+                var cat = e.Channel.ParentId;
+                var ucat = ulong.Parse(BotConfigurator.GetConfig("MainConfig", "BearbeitetCategoryId"));
+                if (cat == ucat)
+                {
+                    return;
+                }
+
+                if (keywords.Contains(e.Message.Content.ToLower()))
+                {
+                    DiscordChannel newcat = e.Guild.GetChannel(ucat);
+                    await e.Channel.ModifyAsync(x => x.Parent = newcat);
+                }
+            }
+            
+        });
+
+    
+
+
+    await Task.CompletedTask;
+    }
+}
